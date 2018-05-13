6b7c47bccc0907b7a393dec00c168dfa62cf90a4
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-13 11:21:28 +0200

    Change js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-var spanEven = $('span:even');
-spanEven.each(function(index, element) {
-	$(spanEven).css('color', 'red');
-});
+$ ('span:even').css('color', 'red');
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
 	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
@@ -10,4 +7,3 @@ paragraphs.each(function(index, element) {
 $('button').click(function() {
 	alert($(this).attr('data-tmp'));
 });
-
